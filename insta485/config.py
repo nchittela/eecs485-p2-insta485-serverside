4f819f30f28af00500d805f0e26dcc259b523fcf
Marisa Wong <mariwong@umich.edu>
2021-09-20 19:37:39 -0400

    Flask tutorial

---
+++ insta485/config.py
@@ -0,0 +1,19 @@
+"""Insta485 development configuration."""
+
+import pathlib
+
+# Root of this application, useful if it doesn't occupy an entire domain
+APPLICATION_ROOT = '/'
+
+# Secret key for encrypting cookies
+SECRET_KEY = b'\x84\x9b6\x13\xcc\x85\x06\x91\xc7&\x08?\x94\xd7\x8c\xe1\xb3\xf9\x86\xeb\x8b{\xd5n'
+SESSION_COOKIE_NAME = 'login'
+
+# File Upload to var/uploads/
+INSTA485_ROOT = pathlib.Path(__file__).resolve().parent.parent
+UPLOAD_FOLDER = INSTA485_ROOT/'var'/'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+# Database file is var/insta485.sqlite3
+DATABASE_FILENAME = INSTA485_ROOT/'var'/'insta485.sqlite3'
