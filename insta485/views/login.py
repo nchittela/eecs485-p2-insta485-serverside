14ff9cdcd85b32f6f7b99611dcbeda4501ba7aa0
Niki Chittela <clniki@umich.edu>
2021-09-24 17:22:53 -0400

    accounts create and delete

--- insta485/views/login.py
+++ insta485/views/login.py
@@ -77,6 +77,69 @@ def account_default_redirect():
             flask.session['username'] = flask.request.form['username']
             print(flask.session['username'])
         return flask.redirect(flask.url_for('show_index'))
-    elif target == None:
+    elif operation == "create":
+        username = flask.request.form['username']
+        password = flask.request.form['password']
+        fullname = flask.request.form['fullname']
+        email = flask.request.form['email']
+        file = flask.request.form['file']
+
+        # Unpack flask object
+        fileobj = flask.request.files["file"]
+        filename = fileobj.filename
+
+        # Compute base name (filename without directory).  We use a UUID to avoid
+        # clashes with existing files, and ensure that the name is compatible with the
+        # filesystem.
+        uuid_basename = "{stem}{suffix}".format(
+            stem=uuid.uuid4().hex,
+            suffix=pathlib.Path(filename).suffix
+        )
+
+        # Save to disk
+        path = insta485.app.config["UPLOAD_FOLDER"]/uuid_basename
+        fileobj.save(path)
+
+        # get login data from database
+        # Connect to database
+        connection = insta485.model.get_db()
+
+        # Query database for user's hashed password
+        cur = connection.execute(
+            "INSERT INTO users(username, fullname, email, filename, password) "
+            "VALUES username = ?, fullname = ?, email = ?, filename = ?, password = ? ",
+            (username, fullname, email, file, password,)
+        )
+
+        #hash password
+        algorithm = 'sha512'
+        salt = uuid.uuid4().hex
+        hash_obj = hashlib.new(algorithm)
+        password_salted = salt + password
+        hash_obj.update(password_salted.encode('utf-8'))
+        password_hash = hash_obj.hexdigest()
+        password_db_string = "$".join([algorithm, salt, password_hash])
+        print(password_db_string)
+
+        return flask.redirect(flask.request.args.get('target'))
+    elif operation == "delete":
+        username = flask.request.form['username']
+
+        # get login data from database
+        # Connect to database
+        connection = insta485.model.get_db()
+
+        # Query database for user's hashed password
+        cur = connection.execute(
+            "DELETE FROM users "
+            "WHERE username = ? ",
+            (username)
+        )
+        flask.session.clear()
+        return flask.redirect(flask.request.args.get('target'))
+    elif operation == "edit_account":
+        
+    elif operation == "update_password":
+    else:
         return flask.redirect(flask.url_for('show_index'))
     return flask.redirect(flask.request.args.get('target'))
